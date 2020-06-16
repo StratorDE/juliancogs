b7097183678d91aba7b5f74a75c9d48f402de3b6
marcel@glaserco.de <marcel@glaserco.de>
2020-06-16 10:28:22 +0200

    Bugfix

--- sponge/sponge.py
+++ sponge/sponge.py
@@ -2,16 +2,18 @@ from redbot.core import commands
 import discord
 
 class Sponge(commands.Cog):
+    
+    msg = ""
 
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        msg = message.content
+        self.msg = message.content
 
 
     @commands.command()
     async def sponge(self, ctx):
-        msg = "".join(c.lower() if i % 2 == 1 else c for i, c in enumerate(msg.upper()))
+        msg = "".join(c.lower() if i % 2 == 1 else c for i, c in enumerate(self.msg.upper()))
         await ctx.send(msg)
         
 
